73b1874240b2c930a5cd749c962e4458c1d6752f
Watson Vieira Costa Junior <watsonmaster@gmail.com>
2015-10-05 22:24:09 -0300

    Envio de mensagens TCP

--- server_mult.py
+++ server_mult.py
@@ -3,14 +3,16 @@
 from time import *
 from socket import *
 import thread
+BUFFER_SIZE = 1024
 
 def listen():
 	sclient = socket(AF_INET, SOCK_DGRAM)
 	sclient.bind(('', 50000))
 	while True:
-		print("Ouvindo conexao")
+		print("\nOuvindo conexao")
 		data, addr = sclient.recvfrom(64)
 		print "Recebido %s %s " % (data, addr)
+		print addr[0]
 
 def server():
 	server = socket(AF_INET, SOCK_DGRAM)
@@ -22,6 +24,36 @@ def server():
 		data = repr(time()) + " cont: " + str(cont) + '\n'
 		server.sendto(data, ('<broadcast>', 50000))
 		sleep(1)
+
+def server_files_list_request(TCP_IP, PORT_IP):
+
+	cmd = "get_file_list"
+    message = ("cmd", cmd)
+    #Envio
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, PORT_IP))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    #return message
+
+#Mensagem 2
+def send_files_list(TCP_IP, PORT_IP):
+    cmd = "get_file_list"
+    message = [[], ("cmd", cmd)]
+    files = ""
+
+    for item in Util.get_files_in_directory(Util.local_files_path):
+        files += item + ";"
+
+    message.append(("files", files))
+    
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, PORT_IP))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+
 
 try:
 	thread.start_new_thread(server, ())
